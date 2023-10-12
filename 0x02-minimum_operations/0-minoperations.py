8918e19ea0f051f3285bbb1710d8d1c38af39d93
satoshiwasHere <skinyanjui100@gmail.com>
2023-10-12 15:40:05 +0000

    first commit

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python3
 """
-method that calculates the fewest number of operations needed to result
+method that calculates the fewest number of operations needed
 """
+import math
+
+
+def factors(n):
+    """
+    factor of n number
+    """
+    mylist = []
+    while n % 2 == 0:
+        mylist.append(2)
+        n = n / 2
+    for y in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % y == 0:
+            mylist.append(y)
+            n = n / y
+    if n > 2:
+        mylist.append(n)
+    return mylist
+
 
 def minOperations(n):
     """
-    Ealuates the fewest number of operations for a particular result
+    calculate the min operations
     """
-    if n <= 0:
+    if type(n) != int or n < 2:
         return 0
-
-    # initialize lsit to store least operations for each position
-    min_op = []
-    y = 1
-    while n != 1:
-        y += 1
-        if n % y == 0:
-            while n % y == 0:
-                n /= y
-                min_op.append(y)
-    return sum(min_op)
+    else:
+        numOperations = sum(factors(n))
+        return int(numOperations)
