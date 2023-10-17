ddce7aff9972de5d1dc3ef73f35078ee4a57d8a5
satoshiwasHere <skinyanjui100@gmail.com>
2023-10-17 19:43:26 +0000

    Pilot commit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,14 +6,9 @@ This script that reads stdin line by line and computes metrics
 
 import sys
 
-cache = {'200': 0,
-         '301': 0,
-         '400': 0,
-         '401': 0,
-         '403': 0,
-         '404': 0,
-         '405': 0,
-         '500': 0}
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+
 
 total_size = 0
 count = 0
